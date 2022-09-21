eef2a13d154f86f1ff0970b7a254898f345a1d62
Lobe Musonda <lobe350@gmail.com>
2022-09-21 23:09:46 +0200

    loaded view scripts

---
+++ build/blocks/auth-modal/frontend.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'bc1c877768f912b0257f');
---
+++ build/blocks/auth-modal/frontend.js
@@ -0,0 +1,11 @@
+/******/ (function() { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!*******************************************!*\
+  !*** ./src/blocks/auth-modal/frontend.js ***!
+  \*******************************************/
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('Dom Ready');
+});
+/******/ })()
+;
+//# sourceMappingURL=frontend.js.map
