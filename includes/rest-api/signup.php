451df841df477a9b46c2d009e836bbfe15542c54
Lobe Musonda <Lo.Musonda@pacra.org.zm>
2022-09-30 12:31:32 +0200

    preventing duplicate users

--- includes/rest-api/signup.php
+++ includes/rest-api/signup.php
@@ -1,8 +1,30 @@
 <?php
 
-function gtp_rest_api_signup_handler()
+function gtp_rest_api_signup_handler($request)
 {
     $response = ['status' => 1];
+    $params = $request->get_json_params();
+
+    if (
+        !isset($params['email'], $params['username'], $params['password']) ||
+        empty($params['email']) ||
+        empty($params['username']) ||
+        empty($params['password'])
+    ) {
+        return $response;
+    }
+
+    $email = sanitize_email($params['email']);
+    $username = sanitize_text_field($params['username']);
+    $password = sanitize_text_field($params['password']);
+
+    if (
+        username_exists($username) ||
+        !is_email($email) ||
+        email_exists($email)
+    ) {
+        return $response;
+    }
 
     $response['status'] = 2;
     return $response;
